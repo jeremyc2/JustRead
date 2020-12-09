5f24053f53c086c559d00e37799b234d419263d8
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2020-12-09 16:39:46 -0600

    gsap

--- landing.js
+++ landing.js
@@ -1,14 +1,29 @@
-var controller = new ScrollMagic.Controller();
+gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log(document.getElementById("cleaner"));
+    let tl = gsap.timeline({
+        // yes, we can add it to an entire timeline!
+        scrollTrigger: {
+          trigger: "#cleaner",
+          pin: true,   // pin the trigger element while active
+          start: "top top", // when the top of the trigger hits the top of the viewport
+          end: "+=500", // end after scrolling 500px beyond the start
+          scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        }
+      });
+    
+    // add animations and labels to the timeline
+    // tl.addLabel("start")
+    //   .from("#cleaner span", {scale: 0.3, rotation:45, autoAlpha: 0})
+    //   .addLabel("color")
+    //   .from("#cleaner span", {backgroundColor: "#28a92b"})
+    //   .addLabel("spin")
+    //   .to("#cleaner span", {rotation: 360})
+    //   .addLabel("end");
 
-    new ScrollMagic.Scene({
-        triggerElement: "#cleaner",
-        triggerHook: 0.9, // show, when scrolled 10% into view
-    })
-    .setClassToggle("#cleaner", "orbit")
-    .addTo(controller);
+    tl.addLabel("start")
+        .to("#cleaner span", {rotation: 360, transformOrigin:"50px 20px"})
+        .to("#cleaner div", {rotation: -360, transformOrigin:"50px 20px"})
 
 });
