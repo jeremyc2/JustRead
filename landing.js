aadf5856be0b96d510aeb8cb6d6f24f22a1879ae
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2020-12-09 13:50:58 -0600

    No commit message

--- landing.js
+++ landing.js
@@ -1,11 +1,17 @@
 var controller = new ScrollMagic.Controller();
 
-new ScrollMagic.Scene({
-    triggerElement: "#cleaner",
-    triggerHook: 0.9, // show, when scrolled 10% into view
-    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-    offset: 50 // move trigger to center of element
-})
-.setClassToggle("#cleaner", "orbit")
-.addIndicators() // add indicators (requires plugin)
-.addTo(controller);
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log(document.getElementById("cleaner"));
+
+    new ScrollMagic.Scene({
+        triggerElement: "#cleaner",
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+    })
+    .setClassToggle("#cleaner", "orbit")
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+});
