e5b9caebf342e575b7d603440a9af9ab9ac5e97d
Jeremy Chandler <goldenmeso@gmail.com>
2020-12-20 18:36:36 -0600

    +

---
+++ random.js
@@ -0,0 +1,37 @@
+function getSentence(index, callback) {
+    return fetch(`sentences/${index}.txt`)
+        .then(response => {
+            response.text().then(text => {
+                callback(index, text);
+            })
+        });
+}
+
+function getRandomSentence(callback) {
+    var i = Math.floor(Math.random() * 7625);
+    getSentence(i, callback);
+}
+
+function getFirstSentence(callback) {
+    var i = 0;
+    getSentence(i, callback);
+}
+
+function getLastSentence(callback) {
+    var i = 7624;
+    getSentence(i, callback);
+}
+
+function getPreviousSentence(current, callback) {
+    if(current <= 0) {
+        return;
+    }
+    getSentence(--current, callback);
+}
+
+function getNextSentence(current, callback) {
+    if(current >= 7624) {
+        return;
+    }
+    getSentence(++current, callback);
+}
