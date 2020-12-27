42065c53bbf78c67b1d9531dced88fc1809f2389
Jeremy Chandler <goldenmeso@gmail.com>
2020-12-27 14:12:06 -0600

    cleanup

---
+++ javascript/help.js
@@ -0,0 +1,29 @@
+var help;
+
+function showHelp() {
+    help.style.display = "block";
+    help.classList.remove("hide");
+    help.classList.add("show");
+}
+
+function hideHelp() {
+
+    help.classList.remove("show");
+
+    // https://css-tricks.com/restart-css-animation/
+    void help.offsetWidth;
+    
+    help.classList.add("hide");
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    help = document.getElementById("help-container");
+    
+    help.addEventListener("animationend", function(e) {
+        if(e.target.classList.contains("hide")) {
+            e.target.style.display = "none";
+        }
+    });
+
+});
