be1138f780075d6f295a98bf393b468e31f92924
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2021-01-11 12:07:58 -0600

    No commit message

--- javascript/state.js
+++ javascript/state.js
@@ -1,13 +1,39 @@
 // https://stackoverflow.com/questions/2136461/use-javascript-to-intercept-all-document-link-clicks
 
-function addState(key, value) {
+class State {
+    
+    constructor(init) {
+        this.params = new URLSearchParams(init);
+    }
 
-}
+    updateDocumentLinks() {
+        [...document.links].forEach(link => {
+            link.href = link.href.replace(/\?.*/,'?' + this.toString());
+        })
+    }
 
-function removeState(key) {
+    add(key, value) {
+        if(this.params.has(key)) {
+            this.params.set(key, value);
+        } else {
+            this.params.append(key, value);
+        }
+    }
 
-}
+    remove(key) {
+        this.params.delete(key);
+    }
 
-function getStateAsQueryString() {
+    has(key) {
+        return this.params.has(key);
+    }
+
+    get(key) {
+        return this.params.get(key);
+    }
+
+    toString() {
+        return this.params.toString();
+    }
 
 }
