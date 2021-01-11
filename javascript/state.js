3bdcf1f937f101b791a7c978b140378d26ac69cd
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2021-01-11 10:57:56 -0600

    No commit message

--- javascript/state.js
+++ javascript/state.js
@@ -1,4 +1,4 @@
-https://stackoverflow.com/questions/2136461/use-javascript-to-intercept-all-document-link-clicks
+// https://stackoverflow.com/questions/2136461/use-javascript-to-intercept-all-document-link-clicks
 
 function addState(key, value) {
 
